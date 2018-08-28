969395cfd71a3a0270ac7d15f56088fe5536b632
dsh <snayp@yandex.ru>
2018-08-28 17:29:00 +0300

    task for Elena

--- src/com/hflabs/Main.java
+++ src/com/hflabs/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String file = "QAtest.txt";
         LineCount lineCount = new LineCount();
         ACharCount aCharCount = new ACharCount();
     }
